dc251b78b9722326ec50617bad72df5e00ee8389
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-02-26 16:26:27 +0100

    improved example 4

--- test_setup_same_client/public/4_example_type_text/js/script.js
+++ test_setup_same_client/public/4_example_type_text/js/script.js
@@ -12,20 +12,40 @@ let myColor = "hsl(" + randomHue + ", 100%, 50%)";
 
 let content = document.getElementById("content");
 
-window.addEventListener("keypress", keypressHandler);
+window.addEventListener("keydown", keydownHandler);
 
-function keypressHandler(e) {
-    // Sending an event 
+
+function keydownHandler(e) {
+    // Prevent browser shortcuts like going back in browser history on backspace
+    e.preventDefault();
+
+    // Sending an event
     socket.emit('serverEvent', {key:e.key, color:myColor});
 }
+
 
 // Incoming events 
 socket.on('serverEvent', function (message) {
     console.log("Incoming event: ", message);
 
-    let newSpan = document.createElement('span');
-    newSpan.style.color = message.color;
-    let newLetter = document.createTextNode(message.key);
-    newSpan.appendChild(newLetter);
-    content.appendChild(newSpan);
+    if (message.key.length == 1) {
+        // If it's a single letter -> create new span element and text
+
+        let newSpan = document.createElement('span');
+        newSpan.style.color = message.color;
+        let newLetter = document.createTextNode(message.key);
+        newSpan.appendChild(newLetter);
+        content.appendChild(newSpan);
+    
+    } else {
+        // Otherwise it's some kind of special letter like Enter or Backspace
+
+        if (message.key == "Backspace") {
+            let lastIndex = content.childNodes.length - 1;
+            if (lastIndex >= 0) {
+                content.childNodes[lastIndex].remove();
+            }
+        }
+    }
+
 });
