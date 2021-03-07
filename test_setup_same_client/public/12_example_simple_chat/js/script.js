9388b4ccdbc59cf71fbe03390bafa55ef13522e6
Hartmut Bohnacker <mail@hartmut-bohnacker.de>
2021-03-07 10:36:02 +0100

    improved simple chat

--- test_setup_same_client/public/12_example_simple_chat/js/script.js
+++ test_setup_same_client/public/12_example_simple_chat/js/script.js
@@ -4,26 +4,30 @@ let socket = io();
 
 // Your script starts here ------------------------------------------------------
 
-let userName = document.querySelector("input#username");
-let chatMessages = document.querySelector(".chat-messages");
-let newMessage = document.querySelector("textarea#newmessage");
+let userNameElement = document.querySelector("#username");
+let userColorElement = document.querySelector("#usercolor");
+let chatMessagesElement = document.querySelector(".chat-messages");
+let newMessageElement = document.querySelector("textarea#newmessage");
 
-userName.value = localStorage.getItem('simpleChatUserName');
+// Get the stored username so you don't have to type it in when reloading
+userNameElement.value = localStorage.getItem('simpleChatUserName');
+let userColor = textToColor(userNameElement.value);
+userColorElement.style.backgroundColor = userColor;
 
 // listen to changes in the textfield to send the message if a return happens
-newMessage.addEventListener("input", function(e) {
+newMessageElement.addEventListener("input", function(e) {
     // in Chrome, a return does NOT give "insertLineBreak" but "insertText" with data = null
     if(e.inputType == "insertLineBreak" || (e.inputType == "insertText" && e.data == null)) {
         // Get text from textarea and remove spaces and return at the end
-        let messageText = newMessage.value.trim();
+        let messageText = newMessageElement.value.trim();
         // Clear textarea
-        newMessage.value = "";
+        newMessageElement.value = "";
 
         if (messageText != "") {
             // Get the name of the user from the input field
-            let name = userName.value;
+            let name = userNameElement.value;
             // Send the message out
-            socket.emit('serverEvent', {type:"message", from:name, text:messageText});
+            socket.emit('serverEvent', {type:"message", from:name, text:messageText, color:userColor});
         }
 
     }
@@ -31,7 +35,9 @@ newMessage.addEventListener("input", function(e) {
 });
 
 // listen to changes of the username to store it locally
-userName.addEventListener("input", function(e) {
+userNameElement.addEventListener("input", function(e) {
+    userColor = textToColor(userNameElement.value);
+    userColorElement.style.backgroundColor = userColor;
     localStorage.setItem('simpleChatUserName', username.value);
 });
 
@@ -45,20 +51,31 @@ socket.on('serverEvent', function (message) {
     console.log("Incoming event: ", message);
 
     if (message.type == "message") {
+        let now = new Date();
         let newDiv = document.createElement("div");
         newDiv.className = "message";
+        newDiv.style.backgroundColor = message.color;
         // Add class "own", if the sending user was me
-        if (message.from == userName.value) {
+        if (message.from == userNameElement.value) {
             newDiv.className += " own";
+            newDiv.innerHTML = "<div class='header'>" + now.toLocaleTimeString() + "</div>"
+        } else {
+            newDiv.innerHTML = "<div class='header'>" + message.from + " at " + now.toLocaleTimeString() + "</div>"
         }
-        let now = new Date();
-        newDiv.innerHTML = "<div class='header'>" + message.from + " at " + now.toLocaleTimeString() + "</div>"
         newDiv.innerHTML += "<div class='text'>" + message.text + "</div>";
 
-        chatMessages.prepend(newDiv);
-        chatMessages.scrollTo(0, 0);
+        chatMessagesElement.prepend(newDiv);
+        chatMessagesElement.scrollTo(0, 0);
     }
 
 });
 
 
+function textToColor(text) {
+    text = text.replaceAll(/[\s\-äöüß]/g, "");
+    let val = parseInt(text, 36);
+    let hue = val % 360;
+    let crom = val % 47;
+    let color = chroma.lch(70, 30 + crom, hue).hex();
+    return color;
+}
