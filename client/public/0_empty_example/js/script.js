ebbdfb8bf9e2efbb5d590fc3542791c5a8aee1e0
StruppeAlexander <alexander.struppe@hfg.design>
2021-03-16 15:44:58 +0100

    Racket Einbindung

--- client/public/0_empty_example/js/script.js
+++ client/public/0_empty_example/js/script.js
@@ -16,24 +16,7 @@ let bounce = false
 let running = true
 let gameover = false
 
-// let blocks = [{
-//   x: 200,
-//   y: 200,
-//   w: 100,
-//   h: 400
-// },
-// {
-//   x: 400,
-//   y: 50,
-//   w: 400,
-//   h: 50
-// },
-// {
-//   x: 400,
-//   y: 200,
-//   w: 50,
-//   h: 50
-// }]
+
 
 class Block {
   constructor(x, y, w, h, color) {
@@ -48,17 +31,12 @@ class Block {
     fill(this.color)
     rect(this.x, this.y, this.w, this.h)
   }
+
+ 
+  
+
 }
-
-let blocks = []
-  // new Block (200,200,100,400,"black"),
-  // new Block (400,50,400,50,"green"),
-  // new Block (400,200,50,50,"pink"),
-  for (i = 0; i < 40; i++){
-    blocks[i] = new Block(i*50 ,50, 48, 20, "lightgreen")
-  }
-
-let racket = new Block(300,300,150,20,"cyan")
+  let racket = new Block(300,300,150,20,"cyan")
 
 // OOP final
 class Ball {
@@ -90,119 +68,35 @@ class Ball {
       this.move.y = -this.move.y
     }
 
-    if (this.hits(racket)) {
-      if ((((this.pos.y + this.size / 2) == racket.y) || // ball above obj
-          (this.pos.y == (racket.y + racket.h)))) {
-
+    if (this.intersect(racket)) {
+      
 
         this.move.y = -this.move.y;
-        this.color = color(Math.random() * 256, Math.random() * 256, Math.random() * 256)
-
-
-      } else if ((((this.pos.x + this.size / 2) == racket.x) || // ball left of obj
-          (this.pos.x == (racket.x + racket.w)))) {
-
-
-        this.color = color(Math.random() * 256, Math.random() * 256, Math.random() * 256)
-        this.move.x = -this.move.x;
-
-      }
+        this.color = color(Math.random() * 256, Math.random() * 256, Math.random() * 256)  
+        
+    }
+      
 }
 
-    blocks.forEach((block, i) => {
-      if (this.hits(block)) {
-        if ((((this.pos.y + this.size / 2) == block.y) || // ball above obj
-            (this.pos.y == (block.y + block.h)))) {
-              block.y = 0;
-              block.x = 0;
-              block.w = 0;
-              block.h = 0;
-          this.move.y = -this.move.y;
-          this.color = color(Math.random() * 256, Math.random() * 256, Math.random() * 256)
+    
 
+  
 
-        } else if ((((this.pos.x + this.size / 2) == block.x) || // ball left of obj
-            (this.pos.x == (block.x + block.w)))) {
-              block.y = 0;
-              block.x = 0;
-              block.w = 0;
-              block.h = 0;
-          this.color = color(Math.random() * 256, Math.random() * 256, Math.random() * 256)
-          this.move.x = -this.move.x;
-
-        }
-
+  intersect(obj) {
+    let left = Math.max(this.pos.x - this.size / 2, obj.x)
+    let right = Math.min(this.pos.x + this.size / 2, obj.x + obj.w)
+    let top = Math.max(this.pos.y - this.size / 2, obj.y)
+    let bottom = Math.min(this.pos.y + this.size / 2, obj.y + obj.h)
+    if (right >= left && bottom >= top) {
+      return {
+        x: left,
+        y: top,
+        w: right - left,
+        h: bottom - top
       }
-    });
-
-  }
-
-  keyPressed() {
-
-    switch (keyCode) {
-
-      // stop start durch leertaste
-      case 32:
-        running = !running;
-        break;
-        //Richtungswechsel
-      case LEFT_ARROW:
-        if (this.move.x > 0) {
-          this.move.x = -this.move.x
-        };
-        console.log('L')
-        break;
-      case RIGHT_ARROW:
-        if (this.move.x < 0) {
-          this.move.x = -this.move.x
-        };
-        console.log('R')
-        break;
-      case 38:
-        if (this.move.y > 0) {
-          this.move.y = -this.move.y
-        };
-        break;
-      case 40:
-        if (this.move.y < 0) {
-          this.move.y = -this.move.y
-        };
-        break;
-
-
-
-        // + für schneller
-      case 187:
-        this.move.x = this.move.x * 1.2;
-        this.move.y = this.move.y * 1.2;
-        break;
-
-        // - für langsamer
-      case 189:
-        this.move.x = this.move.x / 1.2;
-        this.move.y = this.move.y / 1.2;
-        break;
-
-      case 83: // s
-        step = true
-        break;
-
-      default:
-        console.log("KeyCode ist: " + keyCode)
     }
 
-  }
-
-  hits(obj) {
-    return !(
-      (((this.pos.y + this.size / 2) < (obj.y)) || // ball above obj
-        (this.pos.y > (obj.y + obj.h))) || // ball under obj
-      (((this.pos.x + this.size / 2) < obj.x) || // ball left of obj
-        (this.pos.x > (obj.x + obj.w))) // ball right of obj
-    )
-  }
-
-}
+  }}
 
 let ball2 = new Ball({
   x: 10,
@@ -220,38 +114,62 @@ function setup() {
 }
 
 function draw() {
-  if (gameover == false){
+  //if (gameover == false){
     background(50);
-}
-  if (ball2.pos.y > windowHeight - ball2.size / 2){
-    ball2.size = 0;
-    background("red");
-    text("GAME OVER",windowWidth/2, windowHeight/2,);
-    gameover = true;
+
     
-  }
+//}
+  // if (ball2.pos.y > windowHeight - ball2.size / 2){
+  //   ball2.size = 0;
+  //   background("red");
+  //   text("GAME OVER",windowWidth/2, windowHeight/2,);
+  //   gameover = true;
+    
+//}
 
 
 
-  blocks.forEach((block, i) => {
-    block.show()
-  });
+  
+ 
   ball2.update()
   ball2.show()
   step = false
   racket.show()
-  racket.x = mouseX
-  racket.y = 600
-
+  // racket.x = mouseX
+  racket.y = windowHeight - 100;
+  tastendruck();
 }
 
-function keyPressed() {
-  ball2.keyPressed(keyCode)
-  return false;
-}
+
 
 // Incoming events 
 socket.on('serverEvent', function (message) {
 
+  if(message == "Racket1Left"){
+
+    racket.x -= 10
+
+  }
+
+  if(message == "Racket1Right"){
+
+    racket.x += 10
+
+  }
 
 });
+
+function tastendruck() {
+
+  if (keyIsDown(37)) {
+    
+    socket.emit('serverEvent', "Racket1Left")
+    
+  } else if (keyIsDown(39)) {
+    
+    socket.emit('serverEvent', "Racket1Right")
+      
+  } 
+
+
+}
