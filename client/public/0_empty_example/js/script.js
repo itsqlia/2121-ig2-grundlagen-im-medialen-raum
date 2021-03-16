76caa60f5e03a448f9b317da5f8dde48a5f1aa1c
kevincosta1 <76654571+kevincosta1@users.noreply.github.com>
2021-03-16 13:53:44 +0100

    p5Js

--- client/public/0_empty_example/js/script.js
+++ client/public/0_empty_example/js/script.js
@@ -7,7 +7,28 @@ socket.on('connected', function (msg) {
     console.log(msg);
 });
 
-
+let pos = {
+    x: 0,
+    y: 0
+  }
+  let move = {
+    x: 13,
+    y: 22,
+    v: 0
+  }
+  
+  function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background(220);
+    pos.x = pos.x + move.x
+    pos.y = pos.y + move.y
+    
+    rect(mouseX-100,windowHeight -100, 200, 50)
+    ellipse(pos.x,pos.y,50)
+  }
 
 // Incoming events 
 socket.on('serverEvent', function (message) {
