09a8f003d799b61b5ac117ffda6504dc8bb0bb02
kevincosta1 <76654571+kevincosta1@users.noreply.github.com>
2021-03-18 13:11:48 +0100

    tor fix 2

--- client/public/0_empty_example/js/script.js
+++ client/public/0_empty_example/js/script.js
@@ -77,7 +77,7 @@ class Ball {
       this.pos.x = this.pos.x + this.move.x
       this.pos.y = this.pos.y + this.move.y
     }
-    if (this.pos.x < 45 || this.pos.x > WIDTH-35 - this.size / 2) {
+    if (this.pos.x < 30 || this.pos.x > WIDTH-30 - this.size / 2) {
       this.move.x = -this.move.x
     }
     if (this.pos.y < this.size / 2 || this.pos.y > HEIGHT - this.size / 2) {
